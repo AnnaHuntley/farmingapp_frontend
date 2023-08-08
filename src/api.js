d4b82757f8e116fdc8ef8c9fd7a1cc6bbd58e6e0
Anna Samus <annapn32@gmail.com>
2023-08-08 18:42:38 +0100

    proxy

--- src/api.js
+++ src/api.js
@@ -1,8 +1,16 @@
 import axios from 'axios';
 
+const csrfTokenMetaTag = document.querySelector('meta[name="csrf-token"]');
+const csrfToken = csrfTokenMetaTag ? csrfTokenMetaTag.getAttribute('content') : null;
+
 const instance = axios.create({
-  baseURL: '/',
+  baseURL: '/', 
   withCredentials: true,
+  headers: {
+    common: {
+      'X-CSRF-Token': csrfToken,
+    },
+  },
 });
 
 export default instance;
